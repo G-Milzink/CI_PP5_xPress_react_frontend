1c76f11c4ba7d356dbfd712a8824d32bb892edfc
G-Milzink <G.Milzink@gmail.com>
2023-07-18 17:47:45 +0000

    install axios and setup defaults

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL = 'https://xpress-drf-api-96b1c58706ca.herokuapp.com/'
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
+axios.defaults.withCredentials = true
