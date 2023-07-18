3cd186fb49bcc2acb1174939deb22b9ae7daae82
G-Milzink <G.Milzink@gmail.com>
2023-07-18 15:57:25 +0000

    initial App-wide styling (main font/color scheme)

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,13 +7,13 @@ const NavBar = () => {
     return (
         <Container>
             <Navbar expand="md" fixed='top'>
-                <Navbar.Brand><img src={logo} alt='logo' height={45} /></Navbar.Brand>
+                <Navbar.Brand><img src={logo} alt='logo' height={40} /></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto text-left">
-                        <Nav.Link href="#home"><i class="fa-sharp fa-solid fa-house"></i>Home</Nav.Link>
-                        <Nav.Link href="#link"><i class="fa-solid fa-right-to-bracket"></i>LogIn</Nav.Link>
-                        <Nav.Link href="#link"><i class="fa-solid fa-user-plus"></i>SignUp</Nav.Link>
+                        <Nav.Link href="#home"><i class="fa-sharp fa-solid fa-house"></i> Home</Nav.Link>
+                        <Nav.Link href="#link"><i class="fa-solid fa-right-to-bracket"></i> LogIn</Nav.Link>
+                        <Nav.Link href="#link"><i class="fa-solid fa-user-plus"></i> SignUp</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
