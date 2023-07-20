c3f2098ea9672082546968e76422849fb5fa7f9b
G-Milzink <G.Milzink@gmail.com>
2023-07-20 13:23:23 +0000

    setup conditional renderin of post components based on user settings

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -4,6 +4,7 @@ import { useCurrentUser } from '../../contexts/CurrentUserContext';
 import { Card, Media, OverlayTrigger, Tooltip } from 'react-bootstrap';
 import { Link } from 'react-router-dom/cjs/react-router-dom';
 import Avatar from '../../components/Avatar';
+import AudioComponent from '../../components/AudioComponent';
 
 const Post = (props) => {
 
@@ -11,7 +12,7 @@ const Post = (props) => {
         id, owner, profile_id, profile_image,
         comments_count, likes_count, like_id,
         title,
-        include_text, text, excerpt,
+        include_text, excerpt,
         include_image, image, image_description,
         include_audio, audio, audio_description,
         publish, updated_on,
@@ -20,6 +21,11 @@ const Post = (props) => {
 
     const currentUser = useCurrentUser();
     const is_owner = currentUser?.username === owner
+
+    // Check if publish is false, return null (nothing will be rendered)
+    // if (!publish) {
+    //     return null;
+    // }
 
     return (
         <Card className={styles.Post}>
@@ -35,12 +41,24 @@ const Post = (props) => {
                     </div>
                 </Media>
             </Card.Body>
-            <Link to={`/posts/${id}`}>
-                <Card.Img src={image} alt={image_description} />
-            </Link>
+            {title && <Card.Title className='text-center' >{title}</Card.Title>}
+            {include_image &&
+                <Card.Body>
+                    <Link to={`/posts/${id}`}>
+                        <Card.Img src={image} alt={image_description} />
+                    </Link>
+                    {image_description && <Card.Text className='text-center'>{image_description}</Card.Text>}
+                </Card.Body>
+            }
+            
+            {include_audio &&
+                <Card.Body>
+                <AudioComponent src={audio} />
+                {audio_description && <Card.Text className='text-center'>{audio_description}</Card.Text>}
+            </Card.Body>
+            }
             <Card.Body>
-                {title && <Card.Title className='text-center' >{title}</Card.Title>}
-                {excerpt && <Card.Text>{excerpt}</Card.Text>}
+                {include_text && excerpt && <Card.Text>{excerpt}</Card.Text>}
                 <div className={styles.PostBar}>
                     {is_owner ? (
                         <OverlayTrigger placement='top' overlay={<Tooltip>You can not like your own xPression!</Tooltip>}>
