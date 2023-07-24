ed3d35a40548193c7c007535b6c1f8ad29feb9b0
G-Milzink <G.Milzink@gmail.com>
2023-07-24 23:42:01 +0000

    improve spinner visuals

--- src/components/Asset.js
+++ src/components/Asset.js
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Spinner from "react-bootstrap/Spinner";
 import styles from "../styles/Asset.module.css";
 
 const Asset = ({ spinner, src, message }) => {
+    const [showSecondSpinner, setShowSecondSpinner] = useState(false);
+    const [showThirdSpinner, setShowThirdSpinner] = useState(false);
+
+    useEffect(() => {
+        if (spinner) {
+            setShowSecondSpinner(false);
+            setShowThirdSpinner(false);
+
+            const secondSpinnerTimeout = setTimeout(() => {
+                setShowSecondSpinner(true);
+            }, 75);
+
+            const thirdSpinnerTimeout = setTimeout(() => {
+                setShowThirdSpinner(true);
+            }, 150);
+
+            return () => {
+                clearTimeout(secondSpinnerTimeout);
+                clearTimeout(thirdSpinnerTimeout);
+            };
+        }
+    }, [spinner]);
+
     return (
         <div className={`${styles.Asset} p-4`}>
-            {spinner && <Spinner animation="grow" variant="dark" />}
+            <div>
+                {spinner && <Spinner animation="grow" variant="dark" />}
+                {showSecondSpinner && <Spinner animation="grow" variant="dark" />}
+                {showThirdSpinner && <Spinner animation="grow" variant="dark" />}
+            </div>
             {src && <img src={src} alt={message} />}
             {message && <p className="mt-4">{message}</p>}
         </div>
