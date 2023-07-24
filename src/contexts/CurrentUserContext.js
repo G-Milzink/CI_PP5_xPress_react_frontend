058e1bdcea3e930380f4fa842d2a1241194d1062
G-Milzink <G.Milzink@gmail.com>
2023-07-24 15:57:53 +0000

    disable jwt token ccalss for logged out users

--- src/contexts/CurrentUserContext.js
+++ src/contexts/CurrentUserContext.js
@@ -2,6 +2,7 @@ import { createContext, useContext, useEffect, useMemo, useState } from "react";
 import axios from "axios";
 import { axiosReq, axiosRes } from "../api/axiosDefaults";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import { removeTokenTimestamp, shouldRefreshToken } from "../utils/Utils";
 
 export const CurrentUserContext = createContext();
 export const SetCurrentUserContext = createContext();
@@ -30,16 +31,19 @@ export const CurrentUserProvider = ({ children }) => {
     useMemo(() => {
         axiosReq.interceptors.request.use(
             async (config) => {
-                try {
-                    await axios.post('/dj-rest-auth/token/refresh/')
-                } catch (err) {
-                    setCurrentUser((prevCurrentUser) => {
-                        if(prevCurrentUser) {
-                            history.push('/login')
-                        }
-                        return null
-                    })
-                    return config
+                if (shouldRefreshToken()) {
+                    try {
+                        await axios.post('/dj-rest-auth/token/refresh/')
+                    } catch (err) {
+                        setCurrentUser((prevCurrentUser) => {
+                            if (prevCurrentUser) {
+                                history.push('/login')
+                            }
+                            return null
+                        });
+                        removeTokenTimestamp();
+                        return config
+                    }
                 }
                 return config
             },
@@ -57,11 +61,12 @@ export const CurrentUserProvider = ({ children }) => {
                         await axios.post('dj-rest-auth/token/refresh/')
                     } catch (err) {
                         setCurrentUser(prevCurrentUser => {
-                            if(prevCurrentUser) {
+                            if (prevCurrentUser) {
                                 history.push('/login')
                             }
                             return null
-                        })
+                        });
+                        removeTokenTimestamp();
                     }
                     return axios(err.config)
                 }
--- src/pages/auth/LogInForm.js
+++ src/pages/auth/LogInForm.js
@@ -19,6 +19,7 @@ import {
 import axios from "axios";
 import { useSetCurrentUser } from "../../contexts/CurrentUserContext";
 import { useRedirect } from "../../hooks/useRedirect";
+import { setTokenTimestamp } from "../../utils/Utils";
 
 function LogInForm() {
     const setCurrentUser = useSetCurrentUser();
@@ -46,6 +47,7 @@ function LogInForm() {
         try {
             const { data } = await axios.post('/dj-rest-auth/login/', logInData);
             setCurrentUser(data.user);
+            setTokenTimestamp(data);
             history.goBack();
         } catch (err) {
             setErrors(err.response?.data);
