521683cf705a189aab1bee8d0a0cc812000744ea
G-Milzink <G.Milzink@gmail.com>
2024-03-26 13:29:57 +0000

    change database url

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://xpress-drf-api-96b1c58706ca.herokuapp.com/';
+axios.defaults.baseURL = 'https://xpress-api-cc62aa415d83.herokuapp.com/';
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
 
